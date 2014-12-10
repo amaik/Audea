ff481a7120ac59444007b009e655d16779dc82f4
Alexander Speicher <alex.speicher@gmx.net>
2014-12-10 17:59:31 +0100

    WaveShaper files created

---
+++ Source/WaveShaper.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    WaveShaper.h
+    Created: 10 Dec 2014 5:55:13pm
+    Author:  Alex
+
+  ==============================================================================
+*/
+
+#ifndef WAVESHAPER_H_INCLUDED
+#define WAVESHAPER_H_INCLUDED
+
+
+
+
+
+#endif  // WAVESHAPER_H_INCLUDED
